134a12521658db900b5b835a92cd01a5f2a9f07b
Harrsh Patel <dev@harrsh.com>
2023-08-15 12:59:10 +0530

    Added gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,18 @@ import ModelProvider from '@/context/modal.context';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Space_Grotesk } from 'next/font/google';
+import { useRouter } from 'next/router';
+import Script from 'next/script';
+import { Fragment, useEffect } from 'react';
 import { Toaster } from 'react-hot-toast';
+import * as gtag from '../lib/gtag';
+
+declare global {
+    interface Window {
+        dataLayer: any[];
+        gtag: (...args: any[]) => void;
+    }
+}
 
 const font = Space_Grotesk({
     preload: true,
@@ -15,18 +26,54 @@ const font = Space_Grotesk({
 });
 
 export default function App({ Component, pageProps }: AppProps) {
-    return (
-        <ModelProvider>
-            <main className={`${font.className}`}>
-                <Toaster />
+    const router = useRouter();
+    useEffect(() => {
+        const handleRouteChange = (url: any) => {
+            gtag.pageview(url);
+        };
+        router.events.on('routeChangeComplete', handleRouteChange);
+        router.events.on('hashChangeComplete', handleRouteChange);
+        return () => {
+            router.events.off('routeChangeComplete', handleRouteChange);
+            router.events.off('hashChangeComplete', handleRouteChange);
+        };
+    }, [router.events]);
 
-                <Layout>
-                    <Navbar />
-                    <WhatsApp />
-                    <Component {...pageProps} />
-                    <Footer />
-                </Layout>
-            </main>
-        </ModelProvider>
+    return (
+        <Fragment>
+            {/* Google Analytics */}
+            <Script
+                strategy="afterInteractive"
+                src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+            />
+            <Script
+                id="gtag-init"
+                strategy="afterInteractive"
+                dangerouslySetInnerHTML={{
+                    __html: `
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', '${gtag.GA_TRACKING_ID}', {
+                        page_path: window.location.pathname,
+                        });
+                    `
+                }}
+            />
+            {/* Google Analytics */}
+
+            <ModelProvider>
+                <main className={`${font.className}`}>
+                    <Toaster />
+
+                    <Layout>
+                        <Navbar />
+                        <WhatsApp />
+                        <Component {...pageProps} />
+                        <Footer />
+                    </Layout>
+                </main>
+            </ModelProvider>
+        </Fragment>
     );
 }
